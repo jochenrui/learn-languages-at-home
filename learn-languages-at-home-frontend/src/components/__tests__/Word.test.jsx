76e42aa1031a2e36f52ea0d94b17dcb699d09bb9
Jochen Rui <jochen-rui@web.de>
2022-10-03 22:53:03 +0200

    add tests for Word

--- learn-languages-at-home-frontend/src/components/__tests__/Word.test.jsx
+++ learn-languages-at-home-frontend/src/components/__tests__/Word.test.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitFor} from '@testing-library/react'
 import "@testing-library/jest-dom";
-
+import {jest} from '@jest/globals';
+import userEvent from '@testing-library/user-event'
 
 import Word from '../Word'
 
 describe('Word', () => {
-    it('renders correctly', () =>{
-        render(<Word text="Test" onChange={() => {}}/>)
+    describe('when rendering', () => {
+        it('text shows correctly', () =>{
+            render(<Word text="test" onChange={() => {}}/>)
+    
+            expect(screen.getByRole('textbox')).toHaveValue('test')
+        })
+    })
 
-        expect(screen.getByRole('textbox')).toHaveTextContent('Test')
+    describe('When typing', () => {
+        it('onChange is called', async () => {
+            const mockedFunction = {function: (value) => {}}
+            const spy = jest.spyOn(mockedFunction, 'function')
+            render(<Word text="" onChange={mockedFunction.function}/>)
+            await userEvent.type(screen.getByRole('textbox'), 'a')
+            await waitFor(() => {
+                expect(spy).toHaveBeenCalledWith("a")
+            })
+        })
     })
 })
