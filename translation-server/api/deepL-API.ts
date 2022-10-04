decb3e8ec90f7cc69c688822fffd33405bd824e6
Jochen Rui <jochen.rui@cognostics.de>
2022-10-04 22:46:18 +0200

    handle errors

--- translation-server/api/deepL-API.ts
+++ translation-server/api/deepL-API.ts
@@ -1,13 +1,27 @@
 import { deepLClient } from "./axios";
+import { APILimitExceeded, InvalidAPIKey, TooManyRequests } from "./errors";
 
 type LanguageCode = "DE" | "FR" | "EN";
 
 export const translateText = (
   language: LanguageCode,
   text: string
-): Promise<string> => {
+): Promise<string | void> => {
   return deepLClient
     .get("v2/translate", { params: { text, target_lang: language } })
-    .then((res) => res.data)
-    .catch((err) => console.error(err));
+    .then((res) => {
+      console.log(res.status);
+      return "choose";
+    })
+    .catch((error) => {
+      switch (error.response.status) {
+        case 403:
+          throw new InvalidAPIKey();
+        case 429:
+          throw new TooManyRequests();
+        case 456:
+          throw new APILimitExceeded();
+      }
+      throw error;
+    });
 };
--- translation-server/index.ts
+++ translation-server/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import * as deepLTranslator from "./api/deepL-API";
 import bodyParser from "body-parser";
@@ -13,12 +13,21 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.post("/translate", jsonParser, (req: Request, res: Response) => {
-  const { language, text } = req.body.params;
-  deepLTranslator
-    .translateText(language, text)
-    .then((translation) => res.send(translation));
-});
+app.post(
+  "/translate",
+  jsonParser,
+  (req: Request, res: Response, next: NextFunction) => {
+    const { language, text } = req.body.params;
+    deepLTranslator
+      .translateText(language, text)
+      .then((translation) => {
+        res.send(translation);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
