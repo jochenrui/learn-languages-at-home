ca7e7dc7ce0a490ccabb44ee4343ed06193b281e
Jochen Rui <jochen-rui@web.de>
2022-10-04 19:54:59 +0200

    remove unused import

--- learn-languages-at-home-frontend/src/components/__tests__/GapFill.test.jsx
+++ learn-languages-at-home-frontend/src/components/__tests__/GapFill.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {render, screen, waitFor} from '@testing-library/react'
 import "@testing-library/jest-dom";
-import {jest} from '@jest/globals';
 import userEvent from '@testing-library/user-event'
 
 import GapFill from '../GapFill'
