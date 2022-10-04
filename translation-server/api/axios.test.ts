5bd1e8861436882aac3c779495c83cc60580e3ea
Safaa El Ayoubi <safaaelayoubi@NRCDE-L026.fritz.box>
2022-10-04 21:19:34 +0200

    test: axios

--- translation-server/api/axios.test.ts
+++ translation-server/api/axios.test.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
-import deepLClient from "./axios";
+import { jest } from "@jest/globals";
 
 jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 test("should fetch users", () => {
   const response = {};
   const resp = { data: response };
-  axios.get.mockResolvedValue(resp);
+  mockedAxios.create.mockResolvedValue(resp);
 
   // or you could use the following depending on your use case:
-  // axios.get.mockImplementation(() => Promise.resolve(resp))
+  mockedAxios.get.mockImplementation(() => Promise.resolve(resp));
 
   return deepLClient.then((data: any) => expect(data).toEqual(response));
 });
